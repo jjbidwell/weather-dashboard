7d797bdd98f5deadd8128d34285f48f27903bc7b
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-19 20:56:17 -0700

    Messed around with the APIs a lot and couldn't figure out how to get multiple day forcast from openweather. The only way is to have a gps api, but I couldn't find a good one.

--- logic.js
+++ logic.js
@@ -1,34 +1,92 @@
 
-var appID = "a8fe1a1c44677133fbab3264e86bad65";
+var weatherAppID = "a8fe1a1c44677133fbab3264e86bad65";
+var locID = "d5655670775c5fa5776da2665c1a5c6f03f70d5";
 var city = "";
 var state = "";
+var lat;
+var long;
+//var tempArray = [];
 $('.submit-btn').on('click', function(event){
     event.preventDefault()
     
     if($(this).attr('id') === 'city-btn' && $('#city').val().trim() !== ""){
         //Search by City
         city = $('#city').val();        
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + appID;
-            
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + weatherAppID;
+        var locationURL =  "https://api.geocod.io/v1.6/geocode?q=" + city + "&api_key=" + locID;   
                 
     } else if($(this).attr('id') === 'zip-btn' && $('#zip').val().trim() !== ""){
         //Searcg by zip
         var zip = $('#zip').val();
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&units=imperial&appid=" + appID;
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&units=imperial&appid=" + weatherAppID;
+        var locationURL =  "https://api.geocod.io/v1.6/geocode?q=" + zip + "&api_key=" + locID;  
+
     }
 
     
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET",
+        //   }).then(function (data) {
+        //     $("#city-name").text("City: " + data.name);
+        //     $("#current-temp").text("Current temperature: " + data.main.temp.toFixed(1));
+        //     $('#humidity').text("Humidity: " + data.main.humidity + "%");
+        //     $("#hi-temp").text("High temperature: " + data.main.temp_max);
+        //     $("#lo-temp").text("Low temperature: " + data.main.temp_min);
+        // })
+
+
+
         $.ajax({
-            url: queryURL,
+            url: locationURL,
             method: "GET",
-          }).then(function (data) {
-            console.log(data);
-            $("#city-name").text("City: " + data.name);
-            $("#current-temp").text("Current temperature: " + data.main.temp.toFixed(1));
-            $('#humidity').text("Humidity: " + data.main.humidity + "%");
-            $("#hi-temp").text("High temperature: " + data.main.temp_max);
-            $("#lo-temp").text("Low temperature: " + data.main.temp_min);
-        })
+            }).then(function (locationData) {
+              console.log(locationData.results[0].address_components);
+                $("#city-name").text("City: " + locationData.results[0].address_components.city + ", " + locationData.results[0].address_components.state);
+                lat = locationData.results[0].location.lat;
+                long = locationData.results[0].location.lng;
+                var forcastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=minutely,hourly&units=imperial&appid=a8fe1a1c44677133fbab3264e86bad65"
+                $.ajax({
+                url: forcastURL,
+                method: "GET",
+                }).then(function (weatherData) {
+                    // $("#current-temp").text("Current temperature: " + data.main.temp.toFixed(1));
+                    // $('#humidity').text("Humidity: " + data.main.humidity + "%");
+                    // $("#hi-temp").text("High temperature: " + data.main.temp_max);
+                    // $("#lo-temp").text("Low temperature: " + data.main.temp_min);
+
+                // for (var a = 0; a < 40; a += 8){
+                //     for (var i = 0; i < 8 ; i++){
+                //     tempArray.push(forcast.list[a + i].main.temp);
     
+                //     }
+                // }
+                // for (var b = 0; b < tempArray.length; b++)
+    
+    
+                // console.log(data.weather[0].description);
+                // $("#city-name").text("City: " + data.name);
+                // $("#current-temp").text("Current temperature: " + data.main.temp.toFixed(1));
+                // $('#humidity').text("Humidity: " + data.main.humidity + "%");
+                // $("#hi-temp").text("High temperature: " + data.main.temp_max);
+                // $("#lo-temp").text("Low temperature: " + data.main.temp_min);
+            })
+            // for (var a = 0; a < 40; a += 8){
+            //     for (var i = 0; i < 8 ; i++){
+            //     tempArray.push(forcast.list[a + i].main.temp);
+
+            //     }
+            // }
+            // for (var b = 0; b < tempArray.length; b++)
+
+
+            // console.log(data.weather[0].description);
+            // $("#city-name").text("City: " + data.name);
+            // $("#current-temp").text("Current temperature: " + data.main.temp.toFixed(1));
+            // $('#humidity').text("Humidity: " + data.main.humidity + "%");
+            // $("#hi-temp").text("High temperature: " + data.main.temp_max);
+            // $("#lo-temp").text("Low temperature: " + data.main.temp_min);
+        })
+       // console.log(tempArray);
 
 });
