3a36c69aa5aa9f085f15b117e4802d787136f335
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-19 17:41:47 -0700

    Weather data now displays on the page when a city is searched.

--- logic.js
+++ logic.js
@@ -8,13 +8,13 @@ $('.submit-btn').on('click', function(event){
     if($(this).attr('id') === 'city-btn' && $('#city').val().trim() !== ""){
         //Search by City
         city = $('#city').val();        
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appID;
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + appID;
             
                 
     } else if($(this).attr('id') === 'zip-btn' && $('#zip').val().trim() !== ""){
         //Searcg by zip
         var zip = $('#zip').val();
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&appid=" + appID;
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&units=imperial&appid=" + appID;
     }
 
     
@@ -23,6 +23,11 @@ $('.submit-btn').on('click', function(event){
             method: "GET",
           }).then(function (data) {
             console.log(data);
+            $("#city-name").text("City: " + data.name);
+            $("#current-temp").text("Current temperature: " + data.main.temp.toFixed(1));
+            $('#humidity').text("Humidity: " + data.main.humidity + "%");
+            $("#hi-temp").text("High temperature: " + data.main.temp_max);
+            $("#lo-temp").text("Low temperature: " + data.main.temp_min);
         })
     
 
