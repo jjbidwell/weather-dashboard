f99186f490b9a67a3ea5ac85978ff26f3ed0cdc0
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-19 17:12:51 -0700

    Couldn't find openweather state codes so I took out the state selector. Added ajax call successfully. Did some minor styling

--- logic.js
+++ logic.js
@@ -1,4 +1,29 @@
-console.log('JS connected');
-$('h1').on('click', function(){
-    console.log('jQuery connected')
+
+var appID = "a8fe1a1c44677133fbab3264e86bad65";
+var city = "";
+var state = "";
+$('.submit-btn').on('click', function(event){
+    event.preventDefault()
+    
+    if($(this).attr('id') === 'city-btn' && $('#city').val().trim() !== ""){
+        //Search by City
+        city = $('#city').val();        
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appID;
+            
+                
+    } else if($(this).attr('id') === 'zip-btn' && $('#zip').val().trim() !== ""){
+        //Searcg by zip
+        var zip = $('#zip').val();
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&appid=" + appID;
+    }
+
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+          }).then(function (data) {
+            console.log(data);
+        })
+    
+
 });
