164074fd1ec0947ecdbec010c7aa0f98c57b15a7
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-21 19:16:25 -0700

    added some styling and a skeleton that I will use to append the five day forcast. Also added moments js to get today's date and future dates

--- logic.js
+++ logic.js
@@ -2,11 +2,11 @@
 var weatherAppID = "a8fe1a1c44677133fbab3264e86bad65";
 var locID = "iqdeIphOmFTHdvGRonpZrdKkjACvb5Sg";
 var city = "";
-
+var todayDate = moment().format('l');
 var locationURL;
 var lat;
 var long;
-//var tempArray = [];
+//console.log(moment().add(1, 'days').format('l'));
 $('.submit-btn').on('click', function(event){
     event.preventDefault()
     var state = "";
@@ -27,16 +27,7 @@ $('.submit-btn').on('click', function(event){
     }
 
 
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET",
-        //   }).then(function (data) {
-        //     $("#city-name").text("City: " + data.name);
-        //     $("#current-temp").text("Current temperature: " + data.main.temp.toFixed(1));
-        //     $('#humidity').text("Humidity: " + data.main.humidity + "%");
-        //     $("#hi-temp").text("High temperature: " + data.main.temp_max);
-        //     $("#lo-temp").text("Low temperature: " + data.main.temp_min);
-        // })
+
 
 
 
@@ -46,7 +37,7 @@ $('.submit-btn').on('click', function(event){
             }).then(function (locationData) {
                 lat = locationData.results[0].locations[0].displayLatLng.lat;
                 lon = locationData.results[0].locations[0].displayLatLng.lng;
-                var fiveDayURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly&units=imperial&appid=" + weatherAppID;
+                var fiveDayURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=" + weatherAppID;
                 // console.log(locationData.results[0].locations[0].adminArea5);
                 // console.log(locationData.results[0].locations[0].adminArea3);
                 // console.log(locationData.results[0].locations[0].adminArea1);
@@ -56,7 +47,7 @@ $('.submit-btn').on('click', function(event){
                 url: fiveDayURL,
                 method: "GET",
                 }).then(function (fiveDayData) {
-                    console.log(fiveDayData);
+                    console.log(Math.round(fiveDayData.current.temp));
                     for (var i = 1; i < 6 ; i++){
                         console.log(fiveDayData.daily[i].dt);
                     }
