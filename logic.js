8dd340c7758d627c8356dd0ea43074f9c83265d3
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-22 20:37:25 -0700

    Local storage now saves and renders up to ten recent searchees. A city can't show up in the search history more than once. You can click on a city in the search history and it renders the weather from that city. When a search is made it clears the zip code and city fields.

--- logic.js
+++ logic.js
@@ -3,11 +3,25 @@ var weatherAppID = "a8fe1a1c44677133fbab3264e86bad65";
 var locID = "iqdeIphOmFTHdvGRonpZrdKkjACvb5Sg";
 var city = "";
 var state = "";
+var historyArray =[""];
 var todayDate = moment().format('l');
 var locationURL;
+var fiveDayURL;
+var locationResults;
 var lat;
 var long;
 
+
+function render(){
+    if(localStorage.getItem('search history') !== null){
+        historyArray = localStorage.getItem('search history').split(',');
+        for(var a = 0; a < 10 && a < historyArray.length; a++){
+        $('#history-'+ a).text(historyArray[a]);
+        }
+    }
+    
+}
+render();
 //console.log(moment().add(1, 'days').format('l'));
 $('.submit-btn').on('click', function(event){
     event.preventDefault();
@@ -17,12 +31,10 @@ $('.submit-btn').on('click', function(event){
         return;
     }
 
-
     var locationURL = "https://open.mapquestapi.com/geocoding/v1/address?key=iqdeIphOmFTHdvGRonpZrdKkjACvb5Sg&location=victorville,ca"
 
     if($(this).attr('id') === 'city-btn' && $('#city').val().trim() !== ""){
         //Search by City
-        console.log("City Value" + $('#city').val().trim())
         city = $('#city').val();        
         
         if($('option:selected').attr('value') !== "" ){
@@ -30,12 +42,16 @@ $('.submit-btn').on('click', function(event){
         }     
         locationURL = "https://open.mapquestapi.com/geocoding/v1/address?key=" + locID + "&location="+ city + state;
     } else if($(this).attr('id') === 'zip-btn' && $('#zip').val().trim() !== ""){
-
+        //search by Zip
         var zip = $('#zip').val();
         var locationURL = "https://open.mapquestapi.com/geocoding/v1/address?key=" + locID + "&location="+ zip;
+    } else if($(this).attr('class') === 'submit-btn search-p'){
+        //Click on search history
+        city = $(this).text();
+        locationURL = "https://open.mapquestapi.com/geocoding/v1/address?key=" + locID + "&location="+ city;
     }
 
-
+    
 
 
 
@@ -44,28 +60,40 @@ $('.submit-btn').on('click', function(event){
             url: locationURL,
             method: "GET",
             }).then(function (locationData) {
-                var locationResults = locationData.results[0].locations[0]
+                locationResults = locationData.results[0].locations[0]
                 lat = locationResults.displayLatLng.lat;
                 lon = locationResults.displayLatLng.lng;
-                var fiveDayURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=" + weatherAppID;
-                // console.log(locationData.results[0].locations[0].adminArea5);
-                // console.log(locationData.results[0].locations[0].adminArea3);
-                // console.log(locationData.results[0].locations[0].adminArea1);
-                // console.log("Lat " + locationData.results[0].locations[0].displayLatLng.lat);
-                // console.log("Lng " + locationData.results[0].locations[0].displayLatLng.lng);
+                fiveDayURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=" + weatherAppID;
+                console.log(locationData);
+
                 $.ajax({
                 url: fiveDayURL,
                 method: "GET",
                 }).then(function (fiveDayData) {
+                    console.log(fiveDayData)
                     city = locationResults.adminArea5;
                     state = locationResults.adminArea3;
                     if (state !== ""){
-                        $('#city-name').text(city + ", " + state);
-                        $('#search-history').append('<p class="search-p">' + city + ", " + state + '</p>')
-                    } else {
+                        $('#city-name').text(city + " " + state);
+                    } else if (state !== ""){
                         $('#city-name').text(city);
-                        $('#search-history').append('<p class="search-p">' + city + '</p>')
                     }
+
+                    if(historyArray.indexOf(city + " " + state) === -1){
+                       // $('#search-history').append('<p class= "submit-btn search-p">' + city + ", " + state + '</p>');
+                        historyArray.unshift(city + " " + state);
+                    } else if(historyArray.indexOf(city + " " + state) === -1){
+                       // $('#search-history').append('<p class= "submit-btn search-p">' + city + '</p>');
+                        historyArray.unshift(city);
+                    }
+                    
+                    
+                    if (historyArray.length > 10){
+                        historyArray.pop();
+                    }
+                    console.log(historyArray);
+                    localStorage.setItem('search history', historyArray);
+                    render();
                     var currentUV = fiveDayData.current.uvi;
 
                     $('#current-temp').text(Math.round(fiveDayData.current.temp));
@@ -75,7 +103,6 @@ $('.submit-btn').on('click', function(event){
                     $('#wind').text(Math.round(fiveDayData.current.wind_speed));
                     $('#uv').text(currentUV.toFixed(1));
                     $('#weather-icon').attr('src', "http://openweathermap.org/img/wn/" + fiveDayData.current.weather[0].icon + "@2x.png");
-                    console.log(fiveDayData);
                     $('#five-day-forcast').empty();
                     $('#five-day-forcast').css('display', "none");
                     $('#today-weather-data').css('display', "none");
@@ -106,45 +133,17 @@ $('.submit-btn').on('click', function(event){
                         $('#five-day-forcast').delay(500).fadeIn(500);
                     }
 
-                    // $("#current-temp").text("Current temperature: " + data.main.temp.toFixed(1));
-                    // $('#humidity').text("Humidity: " + data.main.humidity + "%");
-                    // $("#hi-temp").text("High temperature: " + data.main.temp_max);
-                    // $("#lo-temp").text("Low temperature: " + data.main.temp_min);
 
-                // for (var a = 0; a < 40; a += 8){
-                //     for (var i = 0; i < 8 ; i++){
-                //     tempArray.push(forcast.list[a + i].main.temp);
     
-                //     }
-                // }
-                // for (var b = 0; b < tempArray.length; b++)
-    
-    
-                // console.log(data.weather[0].description);
-                // $("#city-name").text("City: " + data.name);
-                // $("#current-temp").text("Current temperature: " + data.main.temp.toFixed(1));
-                // $('#humidity').text("Humidity: " + data.main.humidity + "%");
-                // $("#hi-temp").text("High temperature: " + data.main.temp_max);
-                // $("#lo-temp").text("Low temperature: " + data.main.temp_min);
+                
             })
-            // for (var a = 0; a < 40; a += 8){
-            //     for (var i = 0; i < 8 ; i++){
-            //     tempArray.push(forcast.list[a + i].main.temp);
-
-            //     }
-            // }
-            // for (var b = 0; b < tempArray.length; b++)
 
 
-            // console.log(data.weather[0].description);
-            // $("#city-name").text("City: " + data.name);
-            // $("#current-temp").text("Current temperature: " + data.main.temp.toFixed(1));
-            // $('#humidity').text("Humidity: " + data.main.humidity + "%");
-            // $("#hi-temp").text("High temperature: " + data.main.temp_max);
-            // $("#lo-temp").text("Low temperature: " + data.main.temp_min);
+            
         })
-       // console.log(tempArray);
 
 
+$('#city').val('');
+$('#zip').val('')
 
 });
